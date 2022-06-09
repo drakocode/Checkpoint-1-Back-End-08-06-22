b8957ee58bf174f6acace4b3cb2a79e79cd64921
resyakKire <erikkayser843@gmail.com>
2022-06-08 21:13:53 -0300

    avvaliação

---
+++ ConfigServer/src/main/java/com/T2/ConfigServer/ConfigServerApplication.java
@@ -0,0 +1,29 @@
+package com.T2.ConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+
+
+
+
+	/*
+	aplikation.iml pra kd
+
+
+	post no bd
+
+	fazer uma interfaçe IMovies para komunikar kom o
+		katalog
+
+	RETORNAR UMA LISTA KOM OS GENEROS DOS FILMES
+
+	 */
+}
---
+++ Gateaway/src/main/java/com/T2/Gateaway/GateawayApplication.java
@@ -0,0 +1,13 @@
+package com.T2.Gateaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateawayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateawayApplication.class, args);
+	}
+
+}
